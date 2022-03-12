b874c94a81913971639026cae7d9b8c12e4194a0
jaylen.work <jaylen.work@hotmail.com>
2022-03-12 17:35:58 +0800

    feat(test): async test

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 export default {
   preset: 'ts-jest',
   clearMocks: true,
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   coverageProvider: 'v8',
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)']
 }
---
+++ test/async/index.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export const fetchData = () => {
+  return axios.get('http://www.dell-lee.com/react/api/demo.json').then((res) => res.data)
+}
+
+export const fetchError = () => {
+  return axios.get('http://www.dell-lee.com/react/api/error.json').then((res) => res.data)
+}
