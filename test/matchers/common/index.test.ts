c6eb725aa8971f460a335a9d789d8039bdc3b872
jaylen.work <jaylen.work@hotmail.com>
2022-03-12 15:37:23 +0800

    feat(test): matchers::{common, collection}

---
+++ test/matchers/common/index.test.ts
@@ -0,0 +1,17 @@
+describe('common matchers', () => {
+  test('toBe: 我觉得xxx应该是xxx。在你期望的值是某个数据类型的值的时候使用，常用在基础类型值上', () => {
+    const count = 1
+
+    expect(count).toBe(1)
+  })
+
+  test('toEqual: 我觉得xxx应该长成xxx样。在你期望的对象长成某个样子的时候使用，常用在引用类型值上', () => {
+    const obj = {
+      name: 'obj'
+    }
+
+    expect(obj).toEqual({
+      name: 'obj'
+    })
+  })
+})
